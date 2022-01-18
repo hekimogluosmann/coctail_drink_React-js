700e5ba9da22c23324c07c74c9797423e9f1f82d
Ali Hekimoglu <81293597+hekimogluosmann@users.noreply.github.com>
2022-01-18 20:08:28 +1100

    Coctail_Menu_React js

---
+++ build/precache-manifest.0f8388f6c4aba2e035aed6cb01cfe2bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae595997b938699b0a7be8d6f6589ada",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a7ae3cd0739fbdee14c",
+    "url": "/static/css/main.5c529f91.chunk.css"
+  },
+  {
+    "revision": "e1f6309a6f0d3e350015",
+    "url": "/static/js/2.72303b8e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.72303b8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a7ae3cd0739fbdee14c",
+    "url": "/static/js/main.8c0ec4d6.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  },
+  {
+    "revision": "9a3d26451b294fc390aee4ae147a45c7",
+    "url": "/static/media/logo.9a3d2645.svg"
+  }
+]);
